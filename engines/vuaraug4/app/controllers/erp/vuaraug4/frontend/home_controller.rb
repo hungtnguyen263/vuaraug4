1967d4ba99dc448b5ed7867dcb089b9025a13129
Hung Nguyen <tonhungstar@gmail.com>
2019-11-07 08:20:22 +0000

    update

--- engines/vuaraug4/app/controllers/erp/vuaraug4/frontend/home_controller.rb
+++ engines/vuaraug4/app/controllers/erp/vuaraug4/frontend/home_controller.rb
@@ -3,10 +3,11 @@ module Erp
     module Frontend
       class HomeController < Erp::Frontend::FrontendController
         def index
-          #@web_info = Erp::Contacts::Contact.get_main_contact
+          @web_info = Erp::Contacts::Contact.get_main_contact
           
           @sliders = Erp::Banners::Banner.get_home_sliders.order('erp_banners_banners.custom_order asc')
-          @bestseller_products = Erp::Products::Product.get_bestseller_products
+          @bestseller_products = Erp::Products::Product.get_bestseller_products.order(:created_at)
+          @home_about = Erp::Articles::Article.get_all_home_abouts.first
         end
       end
     end
