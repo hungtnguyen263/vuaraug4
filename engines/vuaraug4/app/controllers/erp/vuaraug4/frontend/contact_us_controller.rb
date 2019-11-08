8ce5870680cacaebdd8a0fadc1d25c89e7096363
Hung Nguyen <tonhungstar@gmail.com>
2019-11-08 20:41:53 +0000

    fix

--- engines/vuaraug4/app/controllers/erp/vuaraug4/frontend/contact_us_controller.rb
+++ engines/vuaraug4/app/controllers/erp/vuaraug4/frontend/contact_us_controller.rb
@@ -3,22 +3,22 @@ module Erp
     module Frontend
       class ContactUsController < Erp::Frontend::FrontendController
         def index
-          #@web_info = Erp::Contacts::Contact.get_main_contact
-          #
-          #if params[:contact].present?
-          #  @contact = Erp::Contacts::Contact.new(contact_params)
-          #  if @contact.save and params[:message].present?
-          #    @message = Erp::Contacts::Message.new(message_params)
-          #    @message.contact_id = @contact.id
-          #    if @message.save
-          #      Erp::Contacts::ContactMailer.sending_email_contact(@message).deliver_now
-          #      render :json => {status: 'success', notice: 'Tin nhắn đã được gửi thành công. Chúng tôi sẽ sớm liên hệ lại với Quý vị.'}
-          #    end
-          #  else
-          #    
-          #    render :json => {status: 'error', notice: 'Không thể gửi đi! Vui lòng cung cấp đầy đủ thông tin và thử lại.'}
-          #  end
-          #end
+          @web_info = Erp::Contacts::Contact.get_main_contact
+          
+          if params[:contact].present?
+            @contact = Erp::Contacts::Contact.new(contact_params)
+            if @contact.save and params[:message].present?
+              @message = Erp::Contacts::Message.new(message_params)
+              @message.contact_id = @contact.id
+              if @message.save
+                Erp::Contacts::ContactMailer.sending_email_contact(@message).deliver_now
+                render :json => {status: 'success', notice: 'Tin nhắn đã được gửi thành công. Chúng tôi sẽ sớm liên hệ lại với Quý vị.'}
+              end
+            else
+              
+              render :json => {status: 'error', notice: 'Không thể gửi đi! Vui lòng cung cấp đầy đủ thông tin và thử lại.'}
+            end
+          end
         end
         
         private
